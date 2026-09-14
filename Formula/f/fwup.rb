6f1b0110e9f8ba5fbcc66ac3db2d16fe42ffb0ad
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-14 17:39:27 +0000

    fwup 1.17.0

--- Formula/f/fwup.rb
+++ Formula/f/fwup.rb
@@ -1,8 +1,8 @@
 class Fwup < Formula
   desc "Configurable embedded Linux firmware update creator and runner"
   homepage "https://github.com/fwup-home/fwup"
-  url "https://github.com/fwup-home/fwup/releases/download/v1.16.0/fwup-1.16.0.tar.gz"
-  sha256 "a07b79268247ecee134a916ab928914be2a4ecbac0bc5e5f19212ec36ecb5c21"
+  url "https://github.com/fwup-home/fwup/releases/download/v1.17.0/fwup-1.17.0.tar.gz"
+  sha256 "d2a7ee4986652650270e5c01c13f854bd17fba27b48cdfff69025be92969e01d"
   license "Apache-2.0"
 
   bottle do
