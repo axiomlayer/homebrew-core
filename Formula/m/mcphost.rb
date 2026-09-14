5a112ba362acbd52abe80eaf86b8642ed079f2b3
Michael Cho <michael@michaelcho.dev>
2026-09-13 20:33:50 -0400

    mcphost: remove completions generation

--- Formula/m/mcphost.rb
+++ Formula/m/mcphost.rb
@@ -22,8 +22,6 @@ class Mcphost < Formula
 
   def install
     system "go", "build", *std_go_args(ldflags: "-X main.version=#{version}")
-
-    generate_completions_from_executable(bin/"mcphost", shell_parameter_format: :cobra)
   end
 
   test do
